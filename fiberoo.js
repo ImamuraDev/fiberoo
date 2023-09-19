d7abea8e89bc0593213efbe2e54a5bc5da943e92
ImamuraDev <imamurad@hawaii.edu>
2023-09-19 11:12:15 -1000

    practice wod completion

---
+++ fiberoo.js
@@ -0,0 +1,27 @@
+function fib() {
+  const arr = [0, 1, 1, 2];
+  for (let i = 3; i < 50; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr;
+}
+
+console.log(fib());
+
+function numsToStrings(arr) {
+  const res = _.map(arr, function(point) {
+    return `${point}`;
+  });
+  return res;
+}
+
+console.log(numsToStrings(fib()));
+
+function numEvenNums(arr) {
+  const res = _.filter(arr, function(point) {
+    return point % 2 === 0;
+  });
+  return res.length;
+}
+
+console.log(numEvenNums(fib()));
